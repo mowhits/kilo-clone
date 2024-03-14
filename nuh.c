9a39cba403f7281eb16a6c670eee0c9c35bbcb69
mowhits <lingarkarmo@gmail.com>
2024-03-14 21:27:34 +0530

    screensize

--- nuh.c
+++ nuh.c
@@ -6,17 +6,21 @@
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
-
+#include <sys/ioctl.h>
 /*** defines ***/
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 struct editor_config {
+	int screenrows;
+	int screencols;
 	struct termios orig_termios;
 };
 
 struct editor_config E;
+
+
 /*** terminal ***/
 
 void die(const char *s) {
@@ -71,13 +75,56 @@ char editor_read_key() {
 	return c;
 }
 
+int get_cursor_position(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) // n command asks the terminal for status information, with an argument of 6 to ask for the cursor position
+		return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+	
+	buf[i] = '\0';
+	if (buf[0] != '\x1b' || buf[1] != '[') 
+		return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+	
+	editor_read_key();
+	return -1;
+}
+
+int get_window_size(int *rows, int *cols) {
+	struct winsize ws; // ioctl stores no. of cols and rows in winsize struct ws
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) 
+			return -1; // Moves cursor to bottom right of the window. \x1b[999,999H is not used since unlike with B and C, moving the cursor off-screen is undefined behaviour.
+		return get_cursor_position(rows, cols);
+
+	}
+	
+	else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
 
 /*** output ***/
 
 void editor_draw_rows() {
 	int y;
-	for (y = 0; y < 24; y++) {
-		write(STDOUT_FILENO, "~\r\n", 3);
+	for (y = 0; y < E.screenrows; y++) {
+		write(STDOUT_FILENO, "~", 1);
+		if (y < E.screenrows - 1) 
+			write(STDOUT_FILENO, "\r\n", 2);
+		
 	}
 }
 
@@ -92,6 +139,15 @@ void editor_refresh_screen() {
 	editor_draw_rows();
 	write(STDOUT_FILENO, "\x1b[H", 3);
 }
+
+/*** append buffer ***/
+
+struct abuf {
+	char *b;
+	int len;
+}
+
+#define ABUF_INIT {NULL, 0}
 
 /*** input ***/
 
@@ -108,12 +164,18 @@ void editor_process_keypress() {
 
 /*** init ***/
 
+void init_editor() {
+	if (get_window_size(&E.screenrows, &E.screencols) == -1)
+		die("get_window_size");
+}
+
 int main() {
 	enable_raw_mode();
-
+	init_editor();
 	while (1) {
 		editor_refresh_screen();
 		editor_process_keypress();
 	}
 	return 0;
+
 }
